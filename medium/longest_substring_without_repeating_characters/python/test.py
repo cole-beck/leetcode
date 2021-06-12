11c0ce1ddc02cca52c270abbc51dc4f68da8ce51
cole-beck <beck.cole3@gmail.com>
2021-06-12 03:23:35 -0500

    Make test file more readable

--- medium/longest_substring_without_repeating_characters/python/test.py
+++ medium/longest_substring_without_repeating_characters/python/test.py
@@ -2,9 +2,9 @@ from solution import Solution
 
 
 class TestCase:
-    def __init__(self, case_num, test_string, expected_result):
+    def __init__(self, case_num, input_string, expected_result):
         self.case_num = case_num
-        self.test_string = test_string
+        self.input_string = input_string
         self.expected_result = expected_result
 
 
@@ -12,7 +12,7 @@ def solution_test():
 
     solution = Solution()
 
-    test_data = [
+    test_cases = [
         TestCase(1, '', 0),
         TestCase(2, 'z', 1),
         TestCase(3, 'bbbbb', 1),
@@ -21,18 +21,16 @@ def solution_test():
         TestCase(6, 'abcabcbb', 3),
         TestCase(7, 'abcdefghijklmnopqrstuvwxyz', 26),
         TestCase(8, 'dvdf', 3),
-        TestCase(9, 'abcdba', 4)
+        TestCase(9, 'abcdba', 4),
+        TestCase(10, 'vvvedevevvv', 3)
     ]
 
-    print('Test Case -> Actual Result == Expected Result :: Test String')
+    print('Test Case -> Actual Result == Expected Result')
+    for case in test_cases:
+        actual = solution.lengthOfLongestSubstring(case.input_string)
+        expected = case.expected_result
 
-    for i in range(len(test_data)):
-        case_number = test_data[i].case_num
-        actual = solution.lengthOfLongestSubstring(test_data[i].test_string)
-        expected = test_data[i].expected_result
-        test_string = test_data[i].test_string
-
-        print(f'{case_number} -> {actual} == {expected} :: {test_string}')
-        assert (actual == expected), f'Test Case {case_number} failed on string: {test_string}'
+        print(f'{case.case_num} -> {actual} == {expected}')
+        assert (actual == expected), f'Test Case {case.case_num} failed on string: {case.input_string}'
 
 solution_test()
